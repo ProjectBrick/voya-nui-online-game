16345663c1a85eec7f2cf2b537d31af92c54e94b
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2020-12-30 01:59:31 -0500

    Preliminary markdown readme rendering

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ const {task} = require('gulp');
 const slugify = require('slugify');
 const Jimp = require('jimp');
 const imageSize = require('image-size');
+const marked = require('marked');
 const archiver = require('archiver');
 const tar = require('tar');
 const innosetup = require('innosetup');
@@ -67,6 +68,15 @@ async function shockpkgFile(pkg) {
 	);
 }
 
+function templateStrings(str, vars) {
+	return str.replace(/\$\{([^\}]*)?\}/g, (_, p1) => {
+		if (!vars.hasOwnProperty(p1)) {
+			throw new Error(`Undefined template variable: ${p1}`);
+		}
+		return vars[p1];
+	});
+}
+
 async function readIco(iconset) {
 	const ico = new IconIco();
 	for (const data of await Promise.all([
@@ -106,15 +116,14 @@ async function readIcns(iconset) {
 }
 
 async function pngs2bmps(inDir, outDir) {
-	await Promise.all(
-		(await fse.readdir(inDir))
-			.filter(f => /^[^\.].*\.png$/i.test(f))
-			.map(f => Jimp
-				.read(`${inDir}/${f}`)
-				.then(i => i.write(
-					`${outDir}/${f}`.replace(/\.png$/i, '.bmp')
-				))
-			)
+	await Promise.all((await fse.readdir(inDir))
+		.filter(f => /^[^\.].*\.png$/i.test(f))
+		.map(f => Jimp
+			.read(`${inDir}/${f}`)
+			.then(i => i.write(
+				`${outDir}/${f}`.replace(/\.png$/i, '.bmp')
+			))
+		)
 	);
 }
 
@@ -165,6 +174,30 @@ async function readSourcesFiltered(each) {
 
 async function addLicense(dir) {
 	await fse.copy('LICENSE.txt', `${dir}/LICENSE.txt`);
+}
+
+async function addDocs(dir) {
+	const template = await fse.readFile('docs/template.html', 'utf8');
+	await Promise.all((await fse.readdir('docs'))
+		.filter(f => /^[^\.].*\.md$/i.test(f))
+		.map(f => fse.readFile(`docs/${f}`, 'utf8').then(src => {
+			const body = marked(src, {
+				gfm: true,
+				breaks: true,
+				smartypants: true
+			}).trim();
+			const title = (
+				body.match(/<h\d[^>]*>([\s\S]*)?<\/h\d>/) || []
+			)[1] || '';
+			return fse.writeFile(
+				`${dir}/${f}`.replace(/\.md/i, '.html'),
+				templateStrings(template, {
+					title,
+					body
+				})
+			);
+		}))
+	);
 }
 
 async function makeZip(target, source, name) {
@@ -363,23 +396,16 @@ async function buildBrowser(dir) {
 		'main.css'
 	].map(f => fse.copy(`src/browser/${f}`, `${destData}/${f}`)));
 	const defaultPrefix = 'voyanuionlinegame.';
-	const vars = {
-		LS_PREFIX: process.env.VNOG_LS_PREFIX || defaultPrefix,
-		API_PREFIX: process.env.VNOG_API_PREFIX || defaultPrefix,
-		API_URL: process.env.VNOG_API_URL || '',
-		API_NAME: process.env.VNOG_API_NAME || '',
-		API_LINK: process.env.VNOG_API_LINK || ''
-	};
 	await fse.outputFile(
 		`${destData}/index.html`,
-		(await fse.readFile('src/browser/index.html', 'utf8')).replace(
-			/\$\{([^\}]*)\}/g,
-			(_all, p1) => {
-				const value = vars[p1];
-				if (typeof value !== 'string') {
-					throw new Error(`Undefined template variable: ${p1}`);
-				}
-				return vars[p1];
+		templateStrings(
+			await fse.readFile('src/browser/index.html', 'utf8'),
+			{
+				LS_PREFIX: process.env.VNOG_LS_PREFIX || defaultPrefix,
+				API_PREFIX: process.env.VNOG_API_PREFIX || defaultPrefix,
+				API_URL: process.env.VNOG_API_URL || '',
+				API_NAME: process.env.VNOG_API_NAME || '',
+				API_LINK: process.env.VNOG_API_LINK || ''
 			}
 		)
 	);
@@ -387,6 +413,7 @@ async function buildBrowser(dir) {
 		`${dest}/${appName}.html`,
 		'<meta http-equiv="refresh" content="0;url=data/index.html">\n'
 	);
+	await addDocs(dest);
 	await addLicense(dest);
 }
 
@@ -394,6 +421,7 @@ async function buildWindows(dir, pkg) {
 	const dest = `build/${dir}`;
 	await fse.remove(dest);
 	await bundle(await createBundleWindows(`${dest}/${appName}.exe`), pkg);
+	await addDocs(dest);
 	await addLicense(dest);
 }
 
@@ -401,6 +429,7 @@ async function buildMac(dir, pkg) {
 	const dest = `build/${dir}`;
 	await fse.remove(dest);
 	await bundle(await createBundleMac(`${dest}/${appName}.app`), pkg);
+	await addDocs(dest);
 	await addLicense(dest);
 }
 
@@ -408,6 +437,7 @@ async function buildLinux32(dir, pkg) {
 	const dest = `build/${dir}`;
 	await fse.remove(dest);
 	await bundle(await createBundleLinux32(`${dest}/${appName}`), pkg);
+	await addDocs(dest);
 	await addLicense(dest);
 }
 
@@ -415,6 +445,7 @@ async function buildLinux64(dir, pkg) {
 	const dest = `build/${dir}`;
 	await fse.remove(dest);
 	await bundle(await createBundleLinux64(`${dest}/${appName}`), pkg);
+	await addDocs(dest);
 	await addLicense(dest);
 }
 
